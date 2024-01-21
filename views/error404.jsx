5e1606c330190e2ae44cfaf5a1f6d4933649ee30
vmlayton87 <vmlayton87@gmail.com>
2024-01-21 17:59:50 -0500

    messing with image placement

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,8 @@ function error404 () {
         <Default>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
-                <p>The page you are looking for doesn't exist.</p>              
+                <p>The page you are looking for doesn't exist.</p>  
+                <img src="/public/images/hogwarts404.png" alt="404 image"/>            
             </main>
         </Default>
     )
