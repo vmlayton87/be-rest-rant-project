f49981b537c1ab72038eb9edb7c1063b27dbcf0a
vmlayton87 <vmlayton87@gmail.com>
2024-01-21 16:25:38 -0500

    places index view

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Default = require('../default')
+
+function Index () {
+    return (
+        <Default>
+            <main>
+                <h1>Places Index</h1>
+                               
+            </main>
+        </Default>
+    )
+}
+
+module.exports = Index
