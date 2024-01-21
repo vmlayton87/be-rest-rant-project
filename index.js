f97c1f6fc45f5d2e0a850c037bbd16b6c364df49
vmlayton87 <vmlayton87@gmail.com>
2024-01-21 14:17:11 -0500

    home page

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ require(`dotenv`).config()
 const express = require('express')
 const app = express()
 
+// require jsx for the view engine
+app.set(`view engine`, `jsx`)
+app.engine(`jsx`, require(`express-react-views`).createEngine())
+
 //use the places.js file to create all places pages
 app.use(`/places`, require(`./controllers/places`))
 
 //homepage
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 //wildcard 404 page
---
+++ views/home.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Default = require('./default')
+
+function home () {
+    return (
+        <Default>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </Default>
+    )
+}
+
+module.exports = home
