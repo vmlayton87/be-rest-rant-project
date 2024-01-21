953aefc29be1fa99c311b07770e26819d5ce6a72
vmlayton87 <vmlayton87@gmail.com>
2024-01-21 17:56:17 -0500

    places data

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,13 +1,14 @@
 const React = require('react')
 const Default = require('./default')
 
+
 function error404 () {
+    
     return (
         <Default>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
-                <p>The page you are looking for doesn't exist.</p>
-                
+                <p>The page you are looking for doesn't exist.</p>              
             </main>
         </Default>
     )
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,15 +1,24 @@
 const React = require('react')
 const Default = require('../default')
 
-function Index () {
+function Index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
+    })
     return (
-        <Default>
-            <main>
-                <h1>Places Index</h1>
-                               
-            </main>
-        </Default>
-    )
-}
+      <Default>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Default>
+  )
+  }
+  
 
 module.exports = Index
