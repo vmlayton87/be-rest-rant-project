2b7f376cbda5b0b74e248b13f44e5a49050a91b7
vmlayton87 <vmlayton87@gmail.com>
2024-01-25 04:10:52 -0500

    new form added

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,42 @@ function new_form () {
         <Default>
             <main>
                 <h1>Add a New Place</h1>
+                <form method="POST" action="/places">
+                    <div>
+                        <label htmlFor="name">Place Name</label>
+                        <input 
+                            id="name"
+                            name="name"
+                            required />
+                    </div>
+                    <div>
+                        <label htmlFor="pic">Place Image</label>
+                        <input 
+                            id="pic"
+                            name="pic"
+                            type="url"/>
+                    </div>
+                    <div>
+                        <label htmlFor="city">City</label>
+                        <input 
+                            id="city"
+                            name="city"/>
+                    </div>
+                    <div>
+                        <label htmlFor="state">State</label>
+                        <input 
+                            id="state"
+                            name="state"/>
+                    </div>
+                    <div>
+                        <label htmlFor="cuisine">Cuisine</label>
+                        <input 
+                            id="cuisine"
+                            name="cuisine"
+                            required />
+                    </div>
+                    <input type="submit" value="Add Place"/>
+                </form>
             </main>
         </Default>
     )
