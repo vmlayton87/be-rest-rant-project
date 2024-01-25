8662a6d3118b25efde602cc874d7765ada6e3f37
vmlayton87 <vmlayton87@gmail.com>
2024-01-25 04:53:54 -0500

    changed key name for cuisines to cuisine

--- models/place_list.js
+++ models/place_list.js
@@ -2,12 +2,12 @@ module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
+    cuisine: 'Thai, Pan-Asian',
     pic: '/images/paul-rogers-FlYgsQGoxhA-unsplash.jpg'
   }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
-    cuisines: 'Coffee, Bakery',
+    cuisine: 'Coffee, Bakery',
     pic: '/images/nathan-dumlao-z3em1GBRhvY-unsplash.jpg'
   }]
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -12,7 +12,7 @@ function Index (data) {
             <img src={place.pic} alt={place.name} className="card-img-top object-fit-cover"></img></div>
         <div className="card-body">
           <h5 className="card-title">{place.name}</h5>
-          <p className="card-text">{place.cuisines}</p>
+          <p className="card-text">{place.cuisine}</p>
           <p className="card-text">Located in {place.city}, {place.state}</p>
           <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
