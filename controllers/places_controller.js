47494f31a0aaaf458835e751e42140b446621b0e
vmlayton87 <vmlayton87@gmail.com>
2024-01-25 04:49:11 -0500

    add new place working

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -1,9 +1,8 @@
 const router = require(`express`).Router()
 const places = require(`../models/place_list.js`)
+
 // Get /places
 router.get(`/`, (req, res)=>{
-    
-
     res.render(`places/index`, {places})
 })
 
@@ -15,66 +14,20 @@ router.get(`/new`, (req, res)=> {
 // GET places/:id
 
 //POST /places uses the new form
-router.post( `/`, (req, res)=>{
+router.post('/', (req, res) => {
   console.log(req.body)
-  res.send("POST /places")
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect(`/places`)
 })
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const router = require(`express`).Router()
-// // const places = require(`../models/place_list.js`)
-// let places = [{
-//   name: 'H-Thai-ML',
-//   city: 'Seattle',
-//   state: 'WA',
-//   cuisines: 'Thai, Pan-Asian',
-//   pic: '/images/paul-rogers-FlYgsQGoxhA-unsplash.jpg'
-// }, {
-//   name: 'Coding Cat Cafe',
-//   city: 'Phoenix',
-//   state: 'AZ',
-//   cuisines: 'Coffee, Bakery',
-//   pic: '/images/nathan-dumlao-z3em1GBRhvY-unsplash.jpg'
-// }]
-// // Index page listing all places
-// router.get(`/`, (req, res)=>{
- 
-//   res.render(`places/index`, {places})
-// })
-
-// //New place
-// router.get(`/new`, (req, res)=>{
-//   res.render(`places/new`)
-// })
-
-// //Show individual place
-// router.get('/:id', (req, res) => {
-//   if (places[req.params.id]) {
-//     res.render('places/show', {
-//       place:places[req.params.id]
-//     })
-//   } else {
-//     res.render('error404')
-//   }
-// })
-
-// //adds a new place to the places list
-// router.post(`/`, (req, res)=>{
-//   places.push(req.body)
-//   res.render(`places/index`)
-// })
-
-// module.exports = router
