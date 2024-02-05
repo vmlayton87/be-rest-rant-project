cd5ef55f71aa120500ea9946882249d14078f95a
vmlayton87 <vmlayton87@gmail.com>
2024-02-05 18:21:38 -0500

    comments seed

---
+++ seeders/seed-comments.js
@@ -0,0 +1,33 @@
+const db = require('../models/mongoose_index')
+
+// To use await, we need an async function.
+async function seed() {
+    // Get the place, H-Thai-ML
+    let place = await db.Place.findOne({ name: 'H-Thai-ML' })
+
+    // Create a fake sample comment.
+    let comment = await db.Comment.create({
+        author: 'Famished Fran',
+        rant: false,
+        stars: 5.0,
+        content: 'Wow, simply amazing! Highly recommended!'
+    })
+
+    let comment2 = await db.Comment.create({
+        author: 'Still Hungry',
+        rant: true,
+        stars: 2.0,
+        content: 'Not enough food, small portions. D NOT recommend!'
+    })
+
+    // Add that comment to the place's comment array.
+    place.comments.push(comment.id, comment2.id)
+
+    //save the place now that it has comment
+    await place.save()
+    
+    // Exit the program
+    process.exit()
+}
+
+seed()
